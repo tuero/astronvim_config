bbd447d81b5daa783384c591f8685dbfc56c9060
Jake Tuero <jaketuero94@gmail.com>
2024-05-02 16:41:31 -0600

    filename in winbar

--- lua/plugins/herline.lua
+++ lua/plugins/herline.lua
@@ -40,6 +40,9 @@ return {
                 -- add the file name and icon
                 status.component.file_info {
                 file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+                filename = { fallback = "Empty" },
+                file_icon  = false,
+                filetype = false,
                 file_modified = false,
                 file_read_only = false,
                 hl = status.hl.get_attributes("winbarnc", true),
@@ -54,6 +57,9 @@ return {
                 -- add the file name and icon
                 status.component.file_info { -- add file_info to breadcrumbs
                 file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+                filename = { fallback = "Empty" },
+                file_icon  = false,
+                filetype = false,
                 file_modified = false,
                 file_read_only = false,
                 hl = status.hl.get_attributes("winbar", true),
