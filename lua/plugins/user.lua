eb633e7399fe1dd450010acc71a7882de9fc3554
Jake Tuero <jaketuero94@gmail.com>
2025-12-23 14:23:13 -0700

    roslyn

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -164,4 +164,10 @@ return {
       },
     },
   },
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+        -- your configuration comes here; leave empty for default settings
+    },
 }
