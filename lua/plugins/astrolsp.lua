011dac24a2d442c2e97bf97643b980944fd47686
Jake Tuero <jake@tuero.ca>
2026-09-13 16:00:47 +0000

    dep fix, updates

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -88,8 +88,8 @@ return {
         ["<Leader>uY"] = {
           function() require("astrolsp.toggles").buffer_semantic_tokens() end,
           desc = "Toggle LSP semantic highlight (buffer)",
-          cond = function(client)
-            return client.supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens ~= nil
+          cond = function(client, bufnr)
+            return client.server_capabilities.semanticTokensProvider and vim.lsp.semantic_tokens
           end,
         },
       },
